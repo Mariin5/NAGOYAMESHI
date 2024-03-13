8b09684427b1a8b82c8a0e51def3a128220b6f9e
Mariin5 <bepgaga908@gmail.com>
2024-03-13 12:26:36 +0900

    Deploy

--- myproject/settings.py
+++ myproject/settings.py
@@ -163,13 +163,14 @@ if "STRIPE_PUBLISHABLE_KEY" in os.environ and "STRIPE_API_KEY" in os.environ and
     STRIPE_PUBLISHABLE_KEY  = os.environ["STRIPE_PUBLISHABLE_KEY"]
     STRIPE_API_KEY          = os.environ["STRIPE_API_KEY"]
     STRIPE_PRICE_ID         = os.environ["STRIPE_PRICE_ID"]
+'''
 else:
     from . import secret_settings
 
     STRIPE_API_KEY          = secret_settings.STRIPE_API_KEY
     STRIPE_PUBLISHABLE_KEY  = secret_settings.STRIPE_PUBLISHABLE_KEY
     STRIPE_PRICE_ID         = secret_settings.STRIPE_PRICE_ID
-
+'''
 
 
 

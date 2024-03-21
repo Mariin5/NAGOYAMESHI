d93012c8dd7ccb558fd9ff61e2541f31a3839c06
Mariin5 <bepgaga908@gmail.com>
2024-03-21 15:03:14 +0900

    Deploy

--- nagoyameshi/views.py
+++ nagoyameshi/views.py
@@ -28,6 +28,16 @@ from django.contrib.auth import get_user_model
 from django.views.decorators.csrf import requires_csrf_token
 from django.http import HttpResponseServerError
 
+'''
+エラー500確認用
+'''
+@requires_csrf_token
+def my_customized_server_error(request, template_name='500.html'):
+    import sys
+    from django.views import debug
+    error_html = debug.technical_500_response(request, *sys.exc_info()).content
+    return HttpResponseServerError(error_html)
+
 User = get_user_model()
 
 
@@ -587,13 +597,5 @@ class ReviewView(PremiumMemberMixin,View):
 review   = ReviewView.as_view()
 '''
 
-'''
-エラー500確認用
-'''
-@requires_csrf_token
-def my_customized_server_error(request, template_name='500.html'):
-    import sys
-    from django.views import debug
-    error_html = debug.technical_500_response(request, *sys.exc_info()).content
-    return HttpResponseServerError(error_html)
+
 

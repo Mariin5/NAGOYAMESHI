9e27940b563f8afd7e04da3d30affaffdcdaa597
Mariin5 <bepgaga908@gmail.com>
2024-03-21 14:53:11 +0900

    Deploy

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-f4v1xe4*gzy$fg$z#oa0=a7372__u6#v-f%o4axwfyou6pj9o%
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 #デバッグモードは無効化しておく。
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
